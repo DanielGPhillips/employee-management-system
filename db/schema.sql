2468ae0b5df47b13d1686fa9a75502cd2aeda171
Danny Phillips <danielphillips0101@gmail.com>
2022-02-02 18:33:55 -0800

    Built base structure.

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+    id INT NOT NULL,
+    name VARCHAR(30)
+);
+CREATE TABLE role();
+CREATE TABLE employee();
